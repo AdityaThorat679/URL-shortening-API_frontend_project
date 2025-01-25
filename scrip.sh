e4e7bfb737f1cb26022bca4b0550e45620fdef51
Aditya Thorat <taditya679@gmail.com>
2025-01-25 20:39:22 +0530

    Update scrip.sh

--- scrip.sh
+++ scrip.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MINIKUBE_IP=$(minikube ip)
+MINIKUBE_IP="192.168.49.2"
 HOST_ENTRY="$MINIKUBE_IP url-hosting.com"
 if ! grep -q "$HOST_ENTRY" /etc/hosts; then
   echo "$HOST_ENTRY" | sudo tee -a /etc/hosts
